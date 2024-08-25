c8e3f59b04f82b4a262e73578849d4bc14560b5d
Jyriz <discordketukangan123@gmail.com>
2024-08-25 10:37:18 +0700

    feat: add home.json and update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import Image from "next/image";
 
+import { useState, useEffect } from "react";
+
 import IconInstagram from "./icons/icons.instagram";
 import IconYoutube from "./icons/icons.youtube";
-
-import useWindowWidth from "@/utils/windowWidth";
 
 const nameHeader = () => (
   <p className="font-outline-2-light dark:font-outline-2-dark mx-auto px-10 text-center text-6xl font-extrabold sm:text-8xl lg:px-0 xl:text-9xl">
@@ -16,39 +16,68 @@ interface IHeaderProps {
   windowWidth: number;
 }
 
+interface IHomeData {
+  about: [
+    {
+      title: string;
+      body: { name: string; link: string }[] | string[];
+    },
+  ];
+  exp: [
+    {
+      title: string;
+      body: string | number;
+    },
+  ];
+}
+
 export default function Header({ windowWidth }: IHeaderProps) {
+  const [dataAbout, setDataAbout] = useState<IHomeData>({} as IHomeData);
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("/home.json");
+      const data: IHomeData = await response.json();
+      setDataAbout(data);
+    };
+
+    getData();
+  }, [setDataAbout]);
+
   return (
     <header className="mt-5 px-0 sm:mt-10 lg:px-10 xl:px-28">
       {nameHeader()}
       <div className="mx-auto mt-16 flex max-w-screen-2xl flex-col items-center lg:mt-24 lg:flex-row lg:justify-between">
         {windowWidth >= 1024 && (
           <div className="flex w-1/3 flex-col gap-y-10 text-start">
-            <div>
-              <p className="text-sm font-semibold">BIOGRAPHY</p>
-              <p>
-                I am an SMK student living outside Java, balancing studies with freelancing. This journey has taught me adaptability, discipline, and
-                a commitment to growth.
-              </p>
-            </div>
-            <div>
-              <p className="text-sm font-semibold">SKILLS</p>
-              <p>Video Editing • Pixel Art • Draw Image • Building Minecraft • Design</p>
-            </div>
-            <div>
-              <p className="text-sm font-semibold">SOCIAL MEDIA</p>
-              <div className="flex items-center gap-x-2">
-                <Link href={"https://www.instagram.com/notnezon/"} target="_blank">
-                  <div className="rounded-md bg-lightBlue p-1 dark:bg-blueCus">
-                    <IconInstagram />
-                  </div>
-                </Link>
-                <Link href={"https://www.youtube.com/channel/UCNlSN31zKSfnmjBgRirM6NA"} target="_blank">
-                  <div className="rounded-md bg-lightBlue px-1 dark:bg-blueCus">
-                    <IconYoutube />
-                  </div>
-                </Link>
-              </div>
-            </div>
+            {dataAbout.about &&
+              dataAbout.about.map((x, i) => {
+                if (x.title !== "social media") {
+                  return (
+                    <div key={i}>
+                      <p className="text-sm font-semibold">{x.title.toUpperCase()}</p>
+                      <p>{x.body.toString().replaceAll(",", "")}</p>
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div key={i}>
+                      <p className="text-sm font-semibold">{x.title.toUpperCase()}</p>
+                      <div className="flex items-center gap-x-2">
+                        {(x.body as any).map((e: { name: string; link: string }, i: number) => {
+                          return (
+                            <Link key={i} href={`${e.link}`} target="_blank">
+                              <div className="rounded-md bg-lightBlue p-1 dark:bg-blueCus">
+                                {e.name.toLowerCase() === "instagram" ? <IconInstagram /> : <IconYoutube />}
+                              </div>
+                            </Link>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  );
+                }
+              })}
           </div>
         )}
         <div className="rounded-full border border-black bg-gradient-to-r from-cyan-200 to-blue-400 dark:border-liteLightBlue dark:from-cyan-500 dark:to-blue-700">
@@ -57,47 +86,46 @@ export default function Header({ windowWidth }: IHeaderProps) {
         <div className="my-10 w-full px-10 lg:mt-0 lg:w-1/3 lg:px-0">
           {windowWidth < 1024 && (
             <div className="flex flex-wrap justify-between gap-5 text-start">
-              <div className="w-full md:w-1/3">
-                <p className="text-sm font-semibold">BIOGRAPHY</p>
-                <p>
-                  I am an SMK student living outside Java, balancing studies with freelancing. This journey has taught me adaptability, discipline,
-                  and a commitment to growth.
-                </p>
-              </div>
-              <div className="w-full sm:w-1/2 md:w-1/3">
-                <p className="text-sm font-semibold">SKILLS</p>
-                <p>Video Editing • Pixel Art • Draw Image • Building Minecraft • Design</p>
-              </div>
-              <div>
-                <p className="text-sm font-semibold">SOCIAL MEDIA</p>
-                <div className="flex items-center gap-x-2">
-                  <Link href={"https://www.instagram.com/notnezon/"} target="_blank">
-                    <div className="rounded-md bg-lightBlue p-1 dark:bg-blueCus">
-                      <IconInstagram />
-                    </div>
-                  </Link>
-                  <Link href={"https://www.youtube.com/channel/UCNlSN31zKSfnmjBgRirM6NA"} target="_blank">
-                    <div className="rounded-md bg-lightBlue px-1 dark:bg-blueCus">
-                      <IconYoutube />
-                    </div>
-                  </Link>
-                </div>
-              </div>
+              {dataAbout.about &&
+                dataAbout.about.map((x, i) => {
+                  if (x.title !== "social media") {
+                    return (
+                      <div key={i}>
+                        <p className="text-sm font-semibold">{x.title.toUpperCase()}</p>
+                        <p>{x.body.toString().replaceAll(",", "")}</p>
+                      </div>
+                    );
+                  } else {
+                    return (
+                      <div key={i}>
+                        <p className="text-sm font-semibold">{x.title.toUpperCase()}</p>
+                        <div className="flex items-center gap-x-2">
+                          {(x.body as any).map((e: { name: string; link: string }, i: number) => {
+                            return (
+                              <Link key={i} href={`${e.link}`} target="_blank">
+                                <div className="rounded-md bg-lightBlue p-1 dark:bg-blueCus">
+                                  {e.name.toLowerCase() === "instagram" ? <IconInstagram /> : <IconYoutube />}
+                                </div>
+                              </Link>
+                            );
+                          })}
+                        </div>
+                      </div>
+                    );
+                  }
+                })}
             </div>
           )}
           <div className="mt-5 flex flex-wrap justify-between gap-10 text-start lg:mt-0 lg:flex-col lg:text-end">
-            <div>
-              <p className="text-sm font-semibold">PROJECT DONE</p>
-              <p className="text-3xl font-light md:text-5xl">1000</p>
-            </div>
-            <div>
-              <p className="text-sm font-semibold">WORK EXPERIENCE</p>
-              <p className="text-3xl font-light md:text-5xl">1 YR+</p>
-            </div>
-            <div>
-              <p className="text-sm font-semibold">CLIENT ORDER</p>
-              <p className="text-3xl font-light md:text-5xl">130</p>
-            </div>
+            {dataAbout.exp &&
+              dataAbout.exp.map((x, i) => {
+                return (
+                  <div key={i}>
+                    <p className="text-sm font-semibold">{x.title.toUpperCase()}</p>
+                    <p className="text-3xl font-light md:text-5xl">{x.body}</p>
+                  </div>
+                );
+              })}
           </div>
         </div>
       </div>
